350144ccc3f1ec5ef1077e0576624794651a4611
lugarte <lugarte>
2025-02-09 02:08:23 +0000

    change: first implementation

--- src/main.c
+++ src/main.c
@@ -4,13 +4,23 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 
+#include "system_file.h"
 #include "context.h"
 #include "libft.h"
 
 # define SPACE_SEPARATOR ' '
 
+void free_system_file(void *system_file) {
+    free(((t_system_file *)system_file)->name);
+    free(((t_system_file *)system_file));
+}
+
 void free_token(void *token) {
     free(token);
+}
+
+void free_dummy(void *token) {
+    (void)token;
 }
 
 t_list  *create_token_list(int argc, char *argv[]){
@@ -40,9 +50,9 @@ t_list  *create_token_list(int argc, char *argv[]){
 }
 
 int fill_context(t_context *ctx, t_list *head) {
-    t_list      *tmp;
-    char        *token;
-    struct stat *file_stats;
+    t_list          *tmp;
+    char            *token;
+    t_system_file   *system_file;
 
     while (head) {
 
@@ -54,23 +64,27 @@ int fill_context(t_context *ctx, t_list *head) {
                 if (ft_strncmp(FLAGS, &token[idx], 1))
                     return 1;
                 ctx->flags[(int)token[idx]] = true;
+                free(token);
             }
         }
         // Is a file/directory case
         else {
-            file_stats = ft_calloc(1, sizeof(struct stat));
-            if (file_stats == NULL)
+            system_file = ft_calloc(1, sizeof(t_system_file));
+            if (system_file == NULL)
                 return 1;
 
-            if (stat(token, file_stats)) {
-                free(file_stats);
+            if (stat(token, &system_file->stats)) {
+                free(system_file);
                 return 1;
             }
-            tmp = ft_lstnew(file_stats);
+
+            system_file->name = token;
+            tmp = ft_lstnew(system_file);
+            ft_lstadd_back(&ctx->head, tmp);
         }
         tmp = head;
         head = head->next;
-        ft_lstdelone(tmp, free_token);
+        ft_lstdelone(tmp, free_dummy);
     }
     return 0;
 }
@@ -82,10 +96,12 @@ int process_user_input(int argc, char *argv[], t_context *ctx) {
     if (head == NULL)
         return 1;
 
-    if (fill_context(ctx, head))
+    if (fill_context(ctx, head)) {
+        ft_lstdelone(head, free_token);
         return 1;
+    }
 
-    ft_lstclear(&head, free_token);
+    ft_lstclear(&ctx->head, free_system_file);
     return 0;
 }
 
